c64da0104f58945dba8de51ba182862591d7093d
TKCSeow <tkcseow@gmail.com>
2022-12-08 13:58:25 +0000

    fix redirects with search queries

--- src/utils/general.js
+++ src/utils/general.js
@@ -1,7 +1,8 @@
-export function getRedirectUrl(location) {       
+export function getRedirectUrl(location) { 
+    console.log(location)      
     if (location.pathname === "/") {
         return "/login";
     }
 
-    return `/login?redirect=${location.pathname}`
+    return `/login?redirect=${location.pathname}${location.search}`
 }
